0ef876cdd3cb6c1a7c9e7cd3ce8433c019933bbd
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-07-29 13:27:19 +0900

    Chore: NextJS init and Tailwind + Shadcn/ui init / modify .gitignore for not use zerio install

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <Button bgcolor="white" fontcolor="black" bordercolor="black">
+      Shadcn Button
+    </Button>
+  );
+}
