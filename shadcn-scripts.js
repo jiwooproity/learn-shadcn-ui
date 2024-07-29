428d378c1946c51f6858c10fd3bf53f8eb9f6079
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-07-29 15:30:45 +0900

    Fix: Readline input 종료 및 process 탈출

--- shadcn-scripts.js
+++ shadcn-scripts.js
@@ -5,8 +5,10 @@ const { exec } = require("child_process");
 (async () => {
   const rl = readline.createInterface({ input: stdin, output: stdout });
   const component = await rl.question("생성할 Shadcn/ui 컴포넌트를 입력해 주세요. :");
+
   exec(`yarn dlx shadcn-ui@latest add ${component}`, (err, stdout) => {
     console.log(stdout);
-    return;
+    rl.close();
+    process.exit();
   });
 })();
