dc2046ac575c2abfd665bea0fef0aad5e4f415d5
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-07-29 16:24:38 +0900

    Feat: Input shadcn Component create Variants and apply

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`main-container ${inter.className}`}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,37 @@
+"use client";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/shared/components/ui/accordion";
 import { Button } from "@/shared/components/ui/button";
+import { Input } from "@/shared/components/ui/input";
 
 export default function Home() {
   return (
-    <Button bgcolor="white" fontcolor="black" bordercolor="black">
-      Shadcn Button
-    </Button>
+    <div className="main-container">
+      <div className="main-box">
+        <Input type="text" placeholder="검색어를 입력해 주세요." />
+      </div>
+      <div className="main-box rounded-md border border-solid border-default px-5 py-0">
+        <Accordion type="multiple">
+          <AccordionItem value="First item 1">
+            <AccordionTrigger>Accessible</AccordionTrigger>
+            <AccordionContent>First item 1</AccordionContent>
+          </AccordionItem>
+          <AccordionItem className="border-b-white" value="First item 2">
+            <AccordionTrigger>Accessible</AccordionTrigger>
+            <AccordionContent>First item 2</AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+      <div className="main-box">
+        <Button bgcolor="white" fontcolor="black" bordercolor="black">
+          Shadcn Button
+        </Button>
+      </div>
+    </div>
   );
 }
