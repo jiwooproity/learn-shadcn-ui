1c73745feea11b67c3ea7ca918bb9b1cfcfcc0d0
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-07-30 15:26:02 +0900

    Feat: 패키지 매니저 검증 추가 - 코드 리팩토링 / 불필요한 코드 제거 - 메서드 기능 구분 처리

--- shadcn-scripts.js
+++ shadcn-scripts.js
@@ -1,50 +1,77 @@
 const { readdirSync } = require("fs");
-const path = require("path");
+const { resolve } = require("path");
 const { createInterface } = require("readline/promises");
 const { stdin, stdout } = require("process");
 const { exec } = require("child_process");
+const { aliases } = require("./components.json"); // shadcn/ui conf 파일
 
-const config = require("./components.json");
-
-const OVERWRITE_MESSAGE = "동일한 컴포넌트가 이미 있습니다. 덮어 씌우시겠습니까? (y/n) :";
-const CREATE_COMPONENT = "생성할 Shadcn/ui 컴포넌트를 입력해 주세요. :";
-const CANCELED_MESSAGE = "작업이 취소되었습니다.";
-
-const readdir = () => {
-  const configPath = config.aliases.components.replace("@/", "");
-  const baseUrl = configPath.includes("src") ? "" : "src/"; // @ alias 사용 시에 대한 예외 처리
-  const filePath = path.resolve(__dirname, `${baseUrl}${configPath}/ui`);
-  const fileNames = readdirSync(filePath).map((file) => file.split("."));
-  return fileNames.map((file) => file[0]);
+const MESSAGE = {
+  CREATE: "생성할 Shadcn/ui 컴포넌트를 입력해 주세요. : ",
+  OVERWRITE: "동일한 컴포넌트가 이미 있습니다. 덮어 씌우시겠습니까? (y/n) : ",
+  CANCELED: "작업이 취소되었습니다.",
 };
 
-(async () => {
-  let option = "";
-  const haveComps = readdir();
-  const rl = createInterface({ input: stdin, output: stdout });
-  const component = await rl.question(CREATE_COMPONENT);
+(() => {
+  const rl = createInterface({ input: stdin, output: stdout }); // readline 생성
 
-  // 덮어 씌울지에 대한 분기
-  if (haveComps.includes(component)) {
-    const doOverwrite = await rl.question(OVERWRITE_MESSAGE);
+  // 알림
+  const notice = (msgs, close = true) => {
+    switch (typeof msgs) {
+      case "object":
+        msgs.forEach((msg) => console.log(msg));
+        break;
+      case "string":
+        console.log(msgs);
+      default:
+        break;
+    }
 
-    if (doOverwrite.toLowerCase() !== "y") {
-      console.log(CANCELED_MESSAGE);
+    if (close) {
       rl.close();
       process.exit();
     }
+  };
 
-    option += "--overwrite";
-  }
+  const getUserPkgManage = () => {
+    const filePath = resolve(__dirname, `./`);
+    const files = readdirSync(filePath);
+
+    if (files.includes("package-lock.json")) {
+      return "npx";
+    } else if (files.includes("yarn.lock")) {
+      return "yarn dlx";
+    }
+  };
+
+  // 중복 컴포넌트 검증
+  const validationComp = (componentName) => {
+    const configPath = aliases.components.replace("@/", "");
+    const filePath = resolve(__dirname, `src/${configPath}/ui`);
+    const fileNames = readdirSync(filePath).map((file) => file.split("."));
+    return fileNames.map((file) => file[0]).includes(componentName);
+  };
 
   // 로그 전달
   const output = (err, stdout, stderr) => {
-    console.log(stderr);
-    console.log(stdout);
-    rl.close();
-    process.exit();
+    notice([stderr, stdout]);
   };
 
-  // 컴포넌트 추가 스크립트
-  exec(`yarn dlx shadcn-ui@latest add ${component} ${option}`, output);
+  // 실행 스크립트
+  (async () => {
+    const componentName = await rl.question(MESSAGE.CREATE);
+    let manage = getUserPkgManage();
+    let option = ""; // shadcn/ui CLI 옵션
+
+    // 덮어 씌울지에 대한 분기
+    if (validationComp(componentName)) {
+      if ((await rl.question(MESSAGE.OVERWRITE)) !== "y") {
+        notice(MESSAGE.CANCELED);
+      }
+
+      option += "--overwrite";
+    }
+
+    // 컴포넌트 추가 스크립트
+    exec(`${manage} shadcn-ui@latest add ${componentName} ${option}`, output);
+  })();
 })();
