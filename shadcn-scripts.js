fe4ee2c626ba3fe01700d1a29d486aea67ee57e5
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-07-30 12:22:51 +0900

    Feat: 이미 존재하는 shadcn/ui 컴포넌트에 대한 예외 처리 ( 덮어씌우기 처리 )

--- shadcn-scripts.js
+++ shadcn-scripts.js
@@ -1,14 +1,46 @@
-const readline = require("readline/promises");
+const { readdirSync } = require("fs");
+const path = require("path");
+const { createInterface } = require("readline/promises");
 const { stdin, stdout } = require("process");
 const { exec } = require("child_process");
 
-(async () => {
-  const rl = readline.createInterface({ input: stdin, output: stdout });
-  const component = await rl.question("생성할 Shadcn/ui 컴포넌트를 입력해 주세요. :");
+const OVERWRITE_MESSAGE = "동일한 컴포넌트가 이미 있습니다. 덮어 씌우시겠습니까? (y/n) :";
+const CREATE_COMPONENT = "생성할 Shadcn/ui 컴포넌트를 입력해 주세요. :";
+const CANCELED_MESSAGE = "작업이 취소되었습니다.";
 
-  exec(`yarn dlx shadcn-ui@latest add ${component}`, (err, stdout) => {
+const readdir = () => {
+  const filePath = path.resolve(__dirname, "src/components/common/ui");
+  const fileNames = readdirSync(filePath).map((file) => file.split("."));
+  return fileNames.map((file) => file[0]);
+};
+
+(async () => {
+  let option = "";
+  const haveComps = readdir();
+  const rl = createInterface({ input: stdin, output: stdout });
+  const component = await rl.question(CREATE_COMPONENT);
+
+  // 덮어 씌울지에 대한 분기
+  if (haveComps.includes(component)) {
+    const doOverwrite = await rl.question(OVERWRITE_MESSAGE);
+
+    if (doOverwrite.toLowerCase() !== "y") {
+      console.log(CANCELED_MESSAGE);
+      rl.close();
+      process.exit();
+    }
+
+    option += "--overwrite";
+  }
+
+  // 로그 전달
+  const output = (err, stdout, stderr) => {
+    console.log(stderr);
     console.log(stdout);
     rl.close();
     process.exit();
-  });
+  };
+
+  // 컴포넌트 추가 스크립트
+  exec(`yarn dlx shadcn-ui@latest add ${component} ${option}`, output);
 })();
